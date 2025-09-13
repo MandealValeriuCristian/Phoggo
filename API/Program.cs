26719fdcfd7cf36fd339ea1b0804be1fae163e0c
Valeriu-Cristian Mandeal <valeriu.cristian.mandeal@gmail.com>
2025-09-13 11:14:15 +0300

    29. Creating a generic repository

--- API/Program.cs
+++ API/Program.cs
@@ -14,8 +14,11 @@ builder.Services.AddDbContext<StoreContext>(options =>
 });
 
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
 var app = builder.Build();
+
+// Configure the HTTP request pipeline.
 
 app.MapControllers();
 
