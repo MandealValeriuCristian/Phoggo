2d80e4d77660c2b82884dc9d2cf2ce917bee1896
Valeriu-Cristian Mandeal <valeriu.cristian.mandeal@gmail.com>
2025-09-13 17:43:31 +0300

    36. Adding sorting to the specification

--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -9,6 +9,12 @@ public class SpecificationEvaluator<T> where T: BaseEntity
     {
         if (spec.Criteria != null)
             query = query.Where(spec.Criteria); // x => x.Brand == brand
+
+        if (spec.OrderBy != null)
+            query = query.OrderBy(spec.OrderBy);
+
+        if (spec.OrderByDescending != null)
+            query = query.OrderByDescending(spec.OrderByDescending);
        
         return query;
     }
