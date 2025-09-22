e558decc29c85b5717f7779057b914de78e509d2
Valeriu-Cristian Mandeal <valeriu.cristian.mandeal@gmail.com>
2025-09-22 21:37:14 +0300

    45. Adding pagination part 2

--- Core/Specifications/ProductSpecParams.cs
+++ Core/Specifications/ProductSpecParams.cs
@@ -2,8 +2,19 @@
 
 public class ProductSpecParams
 {
-	private List<string> _brands = [];
+    private const int _maxPageSize = 50;
+    public int PageIndex { get; set; } = 1;
 
+    private int _pageSize = 6;
+
+    public int PageSize
+    {
+        get => _pageSize;
+        set => _pageSize = (value > _maxPageSize) ? _maxPageSize : value;
+    }
+
+
+    private List<string> _brands = [];
 	public List<string> Brands
 	{
 		get => _brands;
