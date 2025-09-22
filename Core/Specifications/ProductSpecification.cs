f271a70e63fdfb7ae38b76c477288f76e88f5fa2
Valeriu-Cristian Mandeal <valeriu.cristian.mandeal@gmail.com>
2025-09-22 22:08:11 +0300

    48. Adding the search functionality

--- Core/Specifications/ProductSpecification.cs
+++ Core/Specifications/ProductSpecification.cs
@@ -5,8 +5,9 @@ namespace Core.Specifications;
 public class ProductSpecification : BaseSpecification<Product>
 {
     public ProductSpecification(ProductSpecParams specParams) : base(x =>
-    (!specParams.Brands.Any() || specParams.Brands.Contains(x.Brand)) &&
-    (!specParams.Types.Any() || specParams.Types.Contains(x.Type))
+    (string.IsNullOrEmpty(specParams.Search) || x.Name.ToLower().Contains(specParams.Search)) &&
+    (specParams.Brands.Count == 0 || specParams.Brands.Contains(x.Brand)) &&
+    (specParams.Types.Count == 0 || specParams.Types.Contains(x.Type))
     )
     {
         ApplyPaging(specParams.PageSize * (specParams.PageIndex - 1), specParams.PageSize);
