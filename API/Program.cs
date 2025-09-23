12c891e40d3814a3764070d12192c37da166a9aa
Valeriu-Cristian Mandeal <valeriu.cristian.mandeal@gmail.com>
2025-09-23 21:17:57 +0300

    52. Exception handling middleware

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using API.Middleware;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
@@ -19,6 +20,8 @@ builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepositor
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 app.MapControllers();
 
